e5b5f76ff7595589626ec9c947533797a63bb5e1
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-08-17 11:45:16 -0700

    second commit

--- apps/admin/src/app/nx-welcome.component.ts
+++ apps/admin/src/app/nx-welcome.component.ts
@@ -433,7 +433,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome store 👋
+            Welcome admin 👋
           </h1>
         </div>
 
