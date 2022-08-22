ae4366ba400fc23155d536659e78e31aa328df7d
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-08-22 15:39:32 -0700

    build: admin - shell and sidebar components

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    DashboardComponent,
+    ShellComponent,
+    SidebarComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
